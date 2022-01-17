76293fc50ee6aec9f352db60f88db8e41282e4f4
Matic Isovski <mi6568@student.uni-lj.si>
2022-01-17 19:57:41 +0100

    graohql

--- api/src/main/java/org/rso/naloga/zapiski/api/v1/CommentApplication.java
+++ api/src/main/java/org/rso/naloga/zapiski/api/v1/CommentApplication.java
@@ -14,8 +14,8 @@ import javax.ws.rs.core.Application;
 @OpenAPIDefinition(info = @Info(title = "Comment service API", version = "v1",
         contact = @Contact(email = "mi5658@student.uni-lj.si"),
         license = @License(name = "dev"), description = "API for managing comments."),
-        servers = @Server(url = "localhost:8080/"))
-@ApplicationPath("/v1")
+        servers = @Server(url = "http://104.45.79.50"))
+@ApplicationPath("v1")
 public class CommentApplication extends Application {
 
 }
